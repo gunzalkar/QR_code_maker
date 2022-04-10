320770b51f12866afe169eef8ea4543f7ce62771
gunzalkar <42808290+gunzalkar@users.noreply.github.com>
2022-04-10 11:42:22 +0530

    Add files via upload

---
+++ qr_code_logo.py
@@ -0,0 +1,65 @@
+import qrcode
+import PIL.Image
+from tkinter import Tk, Label, Entry, Button, Frame, StringVar, W, E, N, S, filedialog
+
+ws = Tk()
+ws.title('QR Code Generator')
+ws.geometry('400x300')
+ws.config(bg='#f25252')
+
+logo = PIL.Image.open('logo.jpg')
+
+def generate_qr():
+    global logo
+    basic = 100
+    width_percentage = (basic/float(logo.size[0]))
+    height_size = int((float(logo.size[1])*float(width_percentage)))
+    logo = logo.resize((basic, height_size), PIL.Image.ANTIALIAS)
+    qrc = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
+
+    qrc.add_data(msg.get())
+    qrc.make()
+
+    if qr_color.get() == "":
+        qr_col = ("#000000")
+    else:
+        qr_col = qr_color.get()
+ 
+    gen_img = qrc.make_image(
+        fill_color=str(qr_col), 
+        bg_color=str("#ffffff"),
+        ).convert('RGBA')
+
+    position = ((gen_img.size[0] - logo.size[0]) // 2, (gen_img.size[1] - logo.size[1]) // 2)
+
+    gen_img.paste(logo, position)
+    gen_img.save(save_name.get()+'.png')
+
+    lbl.config(text='File saved with logo')
+
+frame = Frame(ws, bg='#f25252')
+frame.pack(expand=True)
+
+Label(frame, text='Enter URL ', bg='#f25252').grid(row=0, column=0)
+msg = Entry(frame)
+msg.grid(row=0, column=1)
+
+Label(frame, text='File Name', bg='#f25252').grid(row=1, column=0)
+save_name = Entry(frame)
+save_name.grid(row=1, column=1)
+
+Label (frame, text='QR Color', bg='#f25252').grid(row=2, column=0)
+qr_color = Entry(frame)
+qr_color.grid(row=2, column=1)
+
+btn = Button(
+    frame,
+    text='Generate',
+    command=generate_qr
+)
+btn.grid(row=3, columnspan=3, pady=10)
+
+lbl = Label(ws, fg='green', bg='#f25252')
+lbl.pack()
+
+ws.mainloop()
